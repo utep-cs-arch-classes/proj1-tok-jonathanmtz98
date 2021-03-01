d52187783c7dd16982925c716a0e785452765e24
jonathanmtz98 <72420150+jonathanmtz98@users.noreply.github.com>
2021-02-28 22:30:09 -0700

    Created copy_str function
    Tried to run it in DEVC++ using a while loop. It prints the characters assigned, but then it prints a lot of random characters more. I will need to fix a little of the code.

--- src/WorkingWithStrings2.c
+++ src/WorkingWithStrings2.c
@@ -1,7 +1,14 @@
-void print_tokens(char** str)
-{
-while(*str!="\0") 
-{
+char *copy_str(char *inStr, short len){
+char *p=(char*)malloc(len+1);
+short x=0;
+while(*inStr!=len){
+	*(p+x)=*(inStr+x);
+	x++;
+}
+return p;
+}
+void print_tokens(char** str){
+while(*str!="\0"){
 printf("%s\n",*str);
 str++;
 }
